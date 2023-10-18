de3c562ab1bcd2002a8d2718c88ec43d977d3d7d
Mateus Barreto <matbsantos77@gmail.com>
2023-10-18 00:19:03 -0300

    Adicionar arquivos de projeto.

---
+++ LocaFacil/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LocaFacil.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
