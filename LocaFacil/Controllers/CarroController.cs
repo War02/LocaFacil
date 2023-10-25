2ffaf74c1a8b831f2b58ca299beaa09e1fca2703
55169 <gabriel.okada14@gmail.com>
2023-10-25 01:32:48 -0300

    sessao cadastrar carro.

---
+++ LocaFacil/Controllers/CarroController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LocaFacil.Controllers
+{
+    public class CarroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        } 
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+    }
+}
---
+++ LocaFacil/Repositorio/CarroRepositorio.cs
@@ -0,0 +1,22 @@
+﻿using LocaFacil.Data;
+using LocaFacil.Models;
+
+namespace LocaFacil.Repositorio
+{
+    public class CarroRepositorio : ICarroRepositorio
+    {
+        private readonly BancoContext _context;
+        public CarroRepositorio(BancoContext bancoContext)
+        {
+           this._context = bancoContext;
+        }
+
+        public CarroModel Adicionar(CarroModel carro)
+        {
+            _context.Add(carro);
+            _context.SaveChanges();
+            return carro;
+
+        }
+    }
+}
