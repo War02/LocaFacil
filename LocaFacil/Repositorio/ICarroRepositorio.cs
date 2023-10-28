a74568a080060971047b956da12fbe16aff80d36
55169 <gabriel.okada14@gmail.com>
2023-10-28 01:11:23 -0300

    sessao editar.

--- LocaFacil/Repositorio/ICarroRepositorio.cs
+++ LocaFacil/Repositorio/ICarroRepositorio.cs
@@ -5,6 +5,9 @@ namespace LocaFacil.Repositorio
     public interface ICarroRepositorio
     {
         List<CarroModel> BuscarTodos();
+        CarroModel BuscarPorId(int id);
         CarroModel Adicionar(CarroModel carro);
+        CarroModel Atualizar(CarroModel carro);
+
     }
 }
